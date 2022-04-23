433e8f9a347fc49d22cbcb6df462216cfe95851f
upiw256 <bilqimlb@gmail.com>
2022-04-23 21:50:32 +0700

    membuat header

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>SMAN 1 Margaasih</title>
-        <meta name="description" content="aplikasi SMAN 1 Margaasih" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div><h1>Header</h1></div>
-    </div>
+    <>
+    
+    <Head>
+      <title>SMAN 1 Margaasih</title>
+      <meta name="description" content="aplikasi SMAN 1 Margaasih" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className='bg-utama'><h1>Header</h1></div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        utama: "#EEC373",
+        kedua: "#CA965C",
+        ketiga: "#876445"
+      }
+    },
   },
   plugins: [],
 }
